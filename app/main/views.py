38a2c55c91dcec59fc8fd68ec588b13316cd2609
peter-wairimu <peter.wairimu@student.moringaschool.com>
2021-09-25 23:51:42 +0300

    modified the page

--- app/main/views.py
+++ app/main/views.py
@@ -61,6 +61,27 @@ def update_pic(uname):
     return redirect(url_for('main.home',uname=uname))
 
 
+
+@main.route("/delete-comment/<comment_id>")
+@login_required
+def delete_comment(comment_id):
+    comment =Comment.query.filter_by(id=comment_id).first()
+
+
+    if not comment:
+        flash("Comment does not exist", category="error")
+    elif current_user.id != comment.author and current_user.id != comment.post.author:
+        flash("These comment can not be deleted",category="error")
+    
+    else:
+        db.session.delete(comment)
+        db.session.commit()
+
+
+    return redirect(url_for('main.home'))
+
+
+
 @main.route("/create-blog",methods =['GET','POST'])
 @login_required
 def create_blog():
