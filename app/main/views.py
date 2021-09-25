4421644ee31734db9755003f1623fbb6fbececd0
peter-wairimu <peter.wairimu@student.moringaschool.com>
2021-09-25 17:44:58 +0300

    installed bootstrap

--- app/main/views.py
+++ app/main/views.py
@@ -4,14 +4,32 @@ from flask_login import login_required,current_user
 from .. import db,photos
 import markdown2
 from ..request import get_quote
-
-
+from .forms import  UpdateProfile
+from ..models import User
 
 @main.route("/")
 def home():
     quotes = get_quote()
     return render_template("index.html",quotes=quotes)
 
+
+@main.route('/user/<uname>/update',methods = ['GET','POST'])
+def update_profile(uname):
+    user = User.query.filter_by(username = uname).first()
+    if user is None:
+        abort(404)
+
+    form = UpdateProfile()
+
+    if form.validate_on_submit():
+        user.bio = form.bio.data
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('.profile',uname=user.username))
+
+    return render_template('profile/update.html',form =form)
 
 
 
