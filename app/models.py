2946b323cfe4c4de6886ebed40ee2e51d8498e69
peter-wairimu <peter.wairimu@student.moringaschool.com>
2021-09-25 19:45:57 +0300

    added the post class

--- app/models.py
+++ app/models.py
@@ -25,3 +25,11 @@ class User(db.Model, UserMixin):
 
 
 
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.Text, nullable=False)
+    date_created = db.Column(db.DateTime(timezone=True), default=func.now())
+    author = db.Column(db.Integer, db.ForeignKey(
+        'user.id', ondelete="CASCADE"), nullable=False)
+    comments = db.relationship('Comment', backref='post', passive_deletes=True)
+    likes = db.relationship('Like', backref='post', passive_deletes=True)
